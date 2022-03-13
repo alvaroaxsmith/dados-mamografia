b732a991b34d9f3f69c6766efbfa2385cb05ae23
alvaroun <alvaromachadoferreira@hotmail.com>
2022-03-13 19:13:22 -0300

    organize backend files

---
+++ backend/src/main/java/com/dio_class/devweek/DevweekApplication.java
@@ -0,0 +1,13 @@
+package com.dio_class.devweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevweekApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dio_class/devweek/DevweekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio_class.devweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevweekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
